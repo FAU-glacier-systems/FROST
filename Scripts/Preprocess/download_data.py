e4006871e7402db58f54c4a4fad701e875c2206b
ho11laqe <oskar.herrmann@fau.de>
2025-01-31 14:20:05 +0100

    create README.md and environment.yml

--- Scripts/Preprocess/download_data.py
+++ Scripts/Preprocess/download_data.py
@@ -154,7 +154,8 @@ def crop_hugonnet_to_glacier(rgi_region, date_range, oggm_shop_dataset):
     min_x, max_x = x_coords.min(), x_coords.max()
     min_y, max_y = y_coords.min(), y_coords.max()
 
-    zone_number = int(oggm_shop_dataset.pyproj_srs.split('+')[2].split("=")[1])
+    #zone_number = int(oggm_shop_dataset.pyproj_srs.split('+')[2].split("=")[1])
+    zone_number = 32 #TODO
     zone_letter = "N"  # TODO for south regions
     x_range = np.array([min_x, min_x, max_x, max_x])
     y_range = np.array([min_y, min_y, max_y, max_y])
